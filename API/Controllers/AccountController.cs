23ba7bb75ba240c5bcac445c170e42749665e21b
Mikhail Talapila <mikhailtalapila@yahoo.com>
2025-04-26 10:30:33 -0400

    added identity migrations

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Security.Claims;
+using API.DTOs;
+using API.Extensions;
+using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(SignInManager<AppUser> signInManager): BaseAPIController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult> Register(RegisterDto registerDto)
+    {
+        var user = new AppUser
+        {
+            FirstName = registerDto.FirstName,
+            LastName = registerDto.LastName,
+            Email = registerDto.Email,
+            UserName = registerDto.Email
+        };
+        var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+        if (!result.Succeeded) return BadRequest(result.Errors);
+        return Ok();
+    }
+
+    [Authorize]
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return NoContent();
+    }
+
+    [HttpGet("user-info")]
+    public async Task<ActionResult> GetUserInfo()
+    {
+        if (User.Identity?.IsAuthenticated == false) return NoContent();
+        var user = await signInManager.UserManager.GetUserByEmail(User);
+        return Ok(new 
+        {
+            user.FirstName,
+            user.LastName,
+            user.Email
+        });
+    }
+
+    [HttpGet]
+    public ActionResult GetAuthState()
+    {
+        return Ok(new { IsAuthenticated = User.Identity?.IsAuthenticated ?? false });
+    }
+}
--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Security.Claims;
 using API.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -32,4 +34,14 @@ public class BuggyController: BaseAPIController
     {
         return Ok();
     }
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        return Ok("Hello " + name + " with the id of " + id);
+    }
+
 }

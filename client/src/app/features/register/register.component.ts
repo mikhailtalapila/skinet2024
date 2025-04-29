0559408c52c39f41901744b2850cf6041774f265
Mikhail Talapila <mikhailtalapila@yahoo.com>
2025-04-28 20:44:31 -0400

    register component

--- client/src/app/features/register/register.component.ts
+++ client/src/app/features/register/register.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { AccountService } from '../../core/services/account.service';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../../core/services/snackbar.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCard,
+    MatLabel,
+    MatInput,
+    MatFormField,
+    MatButton,
+    JsonPipe,
+    MatError
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  private fb = inject(FormBuilder);
+  private accountService = inject(AccountService);
+  private router = inject(Router);
+  private snack = inject(SnackbarService)
+  validationErrors?: string[];
 
+  registerForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.required]
+  });
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => {
+        this.snack.success('Registration successful - you can now login');
+        this.router.navigateByUrl('/account/login');
+      },
+      error: errors => this.validationErrors = errors
+    })
+  }
 }
